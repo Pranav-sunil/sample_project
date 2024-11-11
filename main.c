58e339fe3c9426dd8d94c10208d37bb45ca10b14
Pranav <pranav.s@neominds.in>
2024-11-11 11:51:04 +0530

    Basic Calculator. First Commit

---
+++ main.c
@@ -0,0 +1,51 @@
+#include "headers.h"
+
+int main()
+{
+	int first_num, second_num;
+       	float result;
+	char operation;
+
+	printf("Sample Two Number Calculator\n");
+
+	printf("Enter the first number : ");
+	scanf("%d", &first_num );
+
+	printf("Enter the second  number : ");
+	scanf("%d", &second_num );
+
+get_operation:
+
+	__fpurge(stdin);
+	printf("Enter the operator( +, -, x, /) : ");
+	scanf("%c", &operation );
+
+	switch( operation )
+	{
+		case '+':
+			//Addition
+			result = first_num + second_num;
+			break;
+		case '-':
+			//Subtraction
+			result = first_num - second_num;
+			break;
+		case'*':
+		case 'x':
+			//Multiplication
+			result =  first_num * second_num;
+			break;
+		case '/':
+			//Division
+			result = (float)first_num / second_num;
+			break;
+		default:
+			printf("ERROR: Please Input a valid operator\n");
+			goto get_operation;
+			break;
+	}
+
+	printf("The result : %g\n", result );
+	
+	return 0;
+}
